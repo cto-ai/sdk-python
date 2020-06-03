61de08472bd07db2748063ab63f15a9756579b5d
Danielle Brook-Roberge <danielle+remax@hackcapital.com>
2020-06-03 13:30:19 -0700

    Update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cto-ai",
-    version="2.0.2",
+    version="2.1.0",
     author="Danielle Brook-Roberge",
     author_email="danielle@cto.ai",
     description="SDK for The Ops Platform",
