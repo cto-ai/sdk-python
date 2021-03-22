f6176dbf2177680b5724673c132322ac83f2b453
Benjamin Slater <benjamin.ed.slater@gmail.com>
2021-03-22 13:17:04 -0700

    added sdk-user functionality

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cto-ai",
-    version="2.3.1",
+    version="2.3.2",
     author="CTO.ai",
     author_email="devops@cto.ai",
     description="SDK for The Ops Platform",
